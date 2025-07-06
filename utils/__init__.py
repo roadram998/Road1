5a99dd98432147e79c9d9f8061dca554afb7aba1
roadram998 <rodeenramadan85@gmail.com>
2025-07-06 05:15:55 -0300

    Delete utils directory

--- utils/__init__.py
+++
@@ -1,8 +0,0 @@
-from .auth import login_to_account, choose_account
-from .config_manager import load_account_data, save_account_data, load_martingale_settings, save_martingale_settings, get_martingale_settings
-from .helpers import get_pocketoption_credentials
-from .logger import Logger
-from .telegram_bot import setup_telegram, listen_to_signals
-from .martingale_strategy import MartingaleStrategy
-from .trade_modules import trade_execution, trade_preparation, message_handling, trade_globals, trade_utils
-from .redis_client import redis_client
--- utils/trade_modules/__init__.py
+++
@@ -1,8 +0,0 @@
-from .trade_execution import safe_execute_trade  # تحديث إلى safe_execute_trade
-from .trade_preparation import prepare_trade
-from .message_handling import handle_signal
-from .trade_globals import SUPPORTED_SYMBOLS, DEFAULT_DURATION, MINIMUM_PAYOUT, MAX_TIME_DIFF, initialize_driver, quit_driver
-from .trade_utils import validate_symbol, validate_trade_time, format_amount, normalize_symbol, check_payout
-import pandas as pd
-import pandas_ta as ta
-import requests
